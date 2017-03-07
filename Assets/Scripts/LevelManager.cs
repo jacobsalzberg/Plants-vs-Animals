e62830ff1a2f3d660f3a562ff3c7b3c29bef5d80
Jacob Salzberg <jacob1818@gmail.com>
2017-03-07 18:13:37 -0300

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadNextLevelAfter;
+
+    private void Awake()
+    {
+        //print(SceneManager.GetActiveScene().buildIndex);
+        Invoke("LoadTitle", autoLoadNextLevelAfter);
+
+        //SceneManager.LoadScene("01a Start");
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load load: " + name);
+        SceneManager.LoadScene(name);
+    }
+    public void LoadNextLevel()
+    {
+        //print(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("load next level: " + name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //level index
+    }
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("Quit request");
+        Application.Quit();
+
+    }
+}
