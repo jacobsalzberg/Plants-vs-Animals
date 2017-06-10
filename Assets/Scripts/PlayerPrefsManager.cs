cc966fb5c2fa1889f3e4cb6c84c3c1f00f5fb85c
Jacob Salzberg <jacob1818@gmail.com>
2017-06-10 19:02:14 -0300

    playerprefs_test

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerPrefsManager : MonoBehaviour {
 
@@ -9,5 +10,51 @@ public class PlayerPrefsManager : MonoBehaviour {
     const string LEVEL_KEY = "level_unlocked_";
     // example: level_unlocked_2, por isso fica o lugar em branco
 
-	
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume out of range");
+        }
+    }
+        
+        public static float GetMasterVolume ()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+        
+    }
+
+    public static void UnlockLevel (int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1) 
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(),1); // use 1 for true
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked (int level)
+    {
+        int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+        bool isLevelUnlocked = (levelValue == 1);
+
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+            //return bool
+        {
+            return isLevelUnlocked;
+        }
+        else
+        {
+            Debug.LogError("Trying to query level not in build order");
+            return false;
+        }
+    }
 }
+
