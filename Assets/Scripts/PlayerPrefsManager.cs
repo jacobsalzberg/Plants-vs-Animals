cd3343fbc40f2d119be00c838a73fcf59e54d7db
Jacob Salzberg <jacob1818@gmail.com>
2017-06-10 17:33:16 -0300

    playerprefs

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+    // example: level_unlocked_2, por isso fica o lugar em branco
+
+	
+}
