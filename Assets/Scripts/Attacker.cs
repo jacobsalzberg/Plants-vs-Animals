e8401efb2a04ce4a242cf9e3193db3ed74115f2a
Jacob J. Salzberg <jacob1818@gmail.com>
2018-01-03 07:10:00 -0200

    colliders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -8,11 +8,17 @@ public class Attacker : MonoBehaviour {
     [Range(-1f, 1.5f)] public float walkSpeed;
     // Use this for initialization
     void Start () {
-		
+        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidBody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(name + "trigger enter");
+    }
 }
