f71e672d9a03da4fd0d08a8f7aa05a13538f67f4
Jacob Salzberg <jacob1818@gmail.com>
2018-01-06 11:07:45 -0200

    animation events2

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Attacker : MonoBehaviour {
 
 
-    [Range(-1f, 1.5f)] public float walkSpeed;
+    [Range(-1f, 1.5f)] public float currentSpeed;
+    // podia colocar range separado
+
     // Use this for initialization
     void Start () {
         Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
@@ -14,11 +16,17 @@ public class Attacker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(name + "trigger enter");
     }
+
+    public void SetSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
 }
