d7e88ecfab489737fabf7a1f5619ce04412c43ce
Jacob Salzberg <jacob1818@gmail.com>
2018-01-07 09:20:31 -0200

    projectiles_rot

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+    public float speed, damage;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,6 @@ public class Projectile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
 	}
 }
