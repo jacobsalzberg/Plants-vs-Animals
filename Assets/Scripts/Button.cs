b0d584b591c84bb0e8597ab39d0733362d0a4c21
Jacob Salzberg <jacob1818@gmail.com>
2018-01-22 01:44:08 -0200

    +D:/Program Files/Git/docs (webGL build)

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour {
 
     public GameObject defenderPrefab;
     private Button[] buttonArray;
     public static GameObject selectedDefender;
+    private Text costText;
     // static --> só existe um em todo jogo
 
 
 	// Use this for initialization
 	void Start () {
         buttonArray = GameObject.FindObjectsOfType<Button>();
+        costText = GetComponentInChildren<Text>();
+        if (!costText)
+        {
+            Debug.LogWarning(name + "has no cost text");
+        }
+
+        costText.text = defenderPrefab.GetComponent<Defender>().starCost.ToString();
 	}
 	
 	// Update is called once per frame
